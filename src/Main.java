fb1b6919fc7c50c432503c42a60f80c5781cbd17
rahebm <mehmetraheb@gmail.com>
2023-08-02 19:22:17 -0400

    grading code class practice

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        boolean degree = false;
+        double GPA = 3.5;
+        if (degree == true) {
+            System.out.println("Congratulations");
+        }
+        if (GPA > 3.5) {
+            System.out.println("You are eligible for scholarship");
+        } else {
+            System.out.println("You should have studied harder");
+
+        }if (degree==false){
+            System.out.println("you need to get a degree");
+        }
+    }
+}
